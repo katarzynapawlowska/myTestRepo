657b901715216677c0aaa49a4aa26d968136345f
Katarzyna Pawlowska <k.a_pawlowska@wp.pl>
2018-10-10 17:56:20 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,19 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+        public static void main(String[] args) {
+            int result = 10 + 5;
+            System.out.println(result);
+            String name = "Kasia";
+            String lastname = "Pawlowska";
+
+
+
+        }
+}
+
+
